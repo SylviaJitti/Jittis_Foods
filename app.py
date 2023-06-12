5ece67ab66ee8935f84e2265b210a94249d4025f
SylviaJitti <sylviajitti@gmail.com>
2023-06-12 21:06:07 +0100

    FIles updated

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
-from flask import Flask, request, jsonify 
-from models import db, Orders 
+from flask import Flask, request, jsonify, render_template
+from .models import db, Orders 
  
  
  
-app = Flask(__name__) 
+app = Flask(__name__, static_url_path='/static') 
 app.config["SECRET_KEY"] = "dyddhd7373djdjcnnbv0f" 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///testing.sql" 
+app.config["STRICT_SLASHES"] = False
+app.config["DEBUG"] = True
 db.init_app(app)
  
- 
- 
- 
+@app.route("/", methods=["GET"])
+def index():
+    return render_template("index.html")
 @app.route("/add_new_order", methods=["GET", "POST"]) 
 def new_order(): 
     data = request.get_json() 
@@ -26,3 +28,6 @@ def new_order():
          
         # Return a response indicating the success of adding the order 
         return jsonify({"message": "Order placed successfully!"})
+    
+if __name__ == "__main__":
+    app.run()
--- js/script.js
+++ js/script.js
@@ -34,12 +34,14 @@ var swiper = new Swiper(".review-slider", {
     },
 });
 
-import axios from 'axios';
-
 // Make a POST request to add a new order
-const addNewOrder = async (orderData) => {
+/*const addNewOrder = async (orderData) => {
     try {
-      const response = await axios.post('/add_new_order', orderData);
+      const response = await axios.post('/add_new_order', orderData, {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        }
+      });
       console.log(response.data); // Assuming the backend returns a response message
     } catch (error) {
       console.error(error);
@@ -47,13 +49,48 @@ const addNewOrder = async (orderData) => {
   };
 
   // Example order data
-const orderData = {
-    location_id: "123",
-    date: "2023-06-08",
-    plan: "Example Plan",
-    quantity: 5
-  };
+  const orderData = new URLSearchParams();
+  orderData.append('location_id', '123');
+  orderData.append('date', '06-08-2023');
+  orderData.append('plan', 'Example Plan');
+  orderData.append('time', '14:30');
   
   // Call the function to add a new order
   addNewOrder(orderData);
-  
+  */
+import axios from 'axios';
+
+// Function to handle form submission
+const handleFormSubmit = async (event) => {
+  event.preventDefault();
+
+  const location = document.querySelector('#location').value;
+  const date = document.querySelector('#date').value;
+  const time = document.querySelector('#time').value;
+
+  const orderData = {
+    location_id: location,
+    date: date,
+    time: time
+  };
+
+  // Call the function to add a new order
+  addNewOrder(orderData);
+};
+
+// Make a POST request to add a new order
+const addNewOrder = async (orderData) => {
+  try {
+    const response = await axios.post('/add_new_order', orderData, {
+      headers:{
+        'Accept': 'application/json'
+      }
+    });
+    console.log(response.data); // Assuming the backend returns a response message
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// Add event listener to the form submit button
+document.querySelector('#order-form').addEventListener('submit', handleFormSubmit);
